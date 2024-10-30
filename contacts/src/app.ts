4ae4d78326f514539cd0234d29bcaef14fe7f560
Scott Russell <scott.russell@lacework.net>
2024-10-30 10:56:14 -0600

    Adding functionality to update a contact.

--- contacts/src/app.ts
+++ contacts/src/app.ts
@@ -5,52 +5,53 @@ import * as Contacts from "./controllers"
 import morgan from 'morgan';
 import cors from 'cors';
 
-process.on('SIGINT', function() {
-    process.exit();
+process.on('SIGINT', function () {
+	process.exit();
 });
 
 function setupMongoConnection() {
-  const dbUrl = process.env["MONGODB_CONN_STRING"]
-  if (!dbUrl) {
-    console.log("must set MONGODB_CONN_STRING")
-    process.exit();
-  }
-  const dbUser = process.env["MONGODB_USER"]
-  if (!dbUser) {
-    console.log("must set MONGODB_USER")
-    process.exit();
-  }
-  const dbPass = process.env["MONGODB_PASS"]
-  if (!dbPass) {
-    console.log("must set MONGODB_PASS")
-    process.exit();
-  }
+	const dbUrl = process.env["MONGODB_CONN_STRING"]
+	if (!dbUrl) {
+		console.log("must set MONGODB_CONN_STRING")
+		process.exit();
+	}
+	const dbUser = process.env["MONGODB_USER"]
+	if (!dbUser) {
+		console.log("must set MONGODB_USER")
+		process.exit();
+	}
+	const dbPass = process.env["MONGODB_PASS"]
+	if (!dbPass) {
+		console.log("must set MONGODB_PASS")
+		process.exit();
+	}
 
-  mongoose.connect(dbUrl, {user: dbUser, pass: dbPass, connectTimeoutMS: 1000}).then(() => {
-    console.log("Successfully connected to the database");
-  }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-  });
+	mongoose.connect(dbUrl, { user: dbUser, pass: dbPass, connectTimeoutMS: 1000 }).then(() => {
+		console.log("Successfully connected to the database");
+	}).catch(err => {
+		console.log('Could not connect to the database. Exiting now...', err);
+		process.exit();
+	});
 
 }
 
 function createRoutes(app: express.Express) {
-  const urlencodedParser = bodyParser.urlencoded({extended: false})
-  app.post('/contacts', urlencodedParser, Contacts.create);
-  app.delete('/contacts/:contactId', Contacts.deleteOne);
-  app.get('/contacts', Contacts.findAll);
-  app.get('/contacts/:email', Contacts.findOne);
+	const urlencodedParser = bodyParser.urlencoded({ extended: false })
+	app.post('/contacts', urlencodedParser, Contacts.create);
+	app.delete('/contacts/:contactId', Contacts.deleteOne);
+	app.put('/contacts/:contactId', Contacts.updateOne);
+	app.get('/contacts', Contacts.findAll);
+	app.get('/contacts/:email', Contacts.findOne);
 }
 
 function createApp() {
-  const app = express();
-  app.use(cors({origin: "*"}))
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(bodyParser.json())
-  app.use(morgan('combined'))
-  createRoutes(app)
-  return app
+	const app = express();
+	app.use(cors({ origin: "*" }))
+	app.use(bodyParser.urlencoded({ extended: true }))
+	app.use(bodyParser.json())
+	app.use(morgan('combined'))
+	createRoutes(app)
+	return app
 }
 
 // start
